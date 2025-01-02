4ffe1fdeae5e953607f88fd49a2ed7747015407a
Elisabet-Gonzalez <elisabet.j.gonzalez@gmail.com>
2025-01-02 13:28:04 -0600

    Add Quit and Start Button
    Work properly, the buttons are kinda ugly tho

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadSceneAsync(1);
+    } 
+    
+    public void QuitGame()
+    {
+        Debug.Log("Bye Bye");
+        Application.Quit();
+    }
+   
+   
+}
