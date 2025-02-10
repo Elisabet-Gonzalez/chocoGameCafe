d4651f799d089d2d5d482959226e597cf9294741
sophiagiselle <dominguez.sophia1b@gmail.com>
2025-02-10 17:28:38 -0600

    Create DialogueLine.cs
    pasado de verdolaga

---
+++ Assets/Scripts/DialogueLine.cs
@@ -0,0 +1,32 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class DialogueLine : MonoBehaviour
+{
+    public string Speaker { get; set; }
+    public string Text { get; set; }
+    public List<DialogueLine> dialogues = new List<DialogueLine>();
+
+    // sin tanta saliva que no es mamada a la verrrrrga 
+
+    void Start()
+    {
+
+        dialogues.Add(new DialogueLine { Speaker = "Barista", Text = "Hey gorgeous!" });
+        dialogues.Add(new DialogueLine { Speaker = "Costumer", Text = "Hii, how are you?" });
+        dialogues.Add(new DialogueLine { Speaker = "Barista", Text = "Super good, thank you for asking, ready to order?" });
+        dialogues.Add(new DialogueLine { Speaker = "Costumer", Text = "Oh yes!" });
+        dialogues.Add(new DialogueLine { Speaker = "Barista", Text = "Perfect,I'm all ears" });
+        // live love laugh copy paste wey tlj
+
+        foreach (var dialogue in dialogues)
+        {
+            Debug.Log($"{dialogue.Speaker}:{dialogue.Text}");
+        }
+
+    }
+
+}       
+
+  
